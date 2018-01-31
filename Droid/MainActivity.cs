35d0830709b6440957117838b9f7ab90c447c4f5
June Cho <junecho.ucb@gmail.com>
2018-01-30 18:02:36 -0800

    CleanUp MainActivity

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.Runtime;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Content.PM;
 using Lottie.Forms.Droid;
 
 namespace LottieBit.Droid
